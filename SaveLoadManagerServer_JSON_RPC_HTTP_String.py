91fbb3aa2deb89f7fa28a3ce12f5359f8a02ca87
ZHANG-Qidi <grunt4991@gmail.com>
2024-09-10 21:50:13 +0900

    update

--- SaveLoadManagerServer_JSON_RPC_HTTP_String.py
+++ SaveLoadManagerServer_JSON_RPC_HTTP_String.py
@@ -9,6 +9,8 @@ html_string = ('''
 
 <head>
     <title>JSON-RPC Frontend</title>
+    <link rel="shortcut icon" href="#" />
+    <link rel="shortcut icon" href="data:,">
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <style>
